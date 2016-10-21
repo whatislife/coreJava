dd7213b6c0a69fdfe5875041998c56d8b24406ce
whatislife <1053400192@qq.com>
2016-10-21 16:24:32 +0800

    Update Test.java

--- src/com/frankman/base/menu/Test.java
+++ src/com/frankman/base/menu/Test.java
@@ -5,5 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		int num = JobStatus.AUTOCLEANED.getCode();
 		System.out.println(num);
+		
+		System.out.println("sdfsdfsdfsdfsdf")
 	}
 }
