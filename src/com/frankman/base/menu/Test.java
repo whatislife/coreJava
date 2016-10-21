87ffab43d7b2e5d1f06547de7b65c6b8e8af6fef
whatislife <whatislife@163.com>
2016-10-21 16:34:28 +0800

    水电费水电费

--- src/com/frankman/base/menu/Test.java
+++ src/com/frankman/base/menu/Test.java
@@ -5,10 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		int num = JobStatus.AUTOCLEANED.getCode();
 		System.out.println(num);
-<<<<<<< HEAD
-		System.out.println("asdfsdfsdf");=======
 		
 		System.out.println("songjian");
->>>>>>> branch 'master' of https://github.com/whatislife/coreJava.git
 	}
 }
