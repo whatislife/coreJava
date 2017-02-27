9f9cc25d1cffb8fa170a5cd73c26381d8aa78861
whatislife <whatis_life@163.com>
2017-02-27 22:11:12 +0800

    再次提交

--- src/com/frankman/api/GetWeatherSignAndTypeList.java
+++ src/com/frankman/api/GetWeatherSignAndTypeList.java
@@ -17,7 +17,7 @@ public class GetWeatherSignAndTypeList {
      * @return
      */
     public static String excute(){
-    	return PureNetUtil.get(URL);//调用工具类获取接口数据
+    	return PureNetUtil.get(URL);//调用工具类获取接口数据nihao
         
         
         
